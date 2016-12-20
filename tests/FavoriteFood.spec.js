057efacb93b283c324099f68add65ccf84808ec2
jbok4 <jbok4@aol.com>
2016-12-19 22:32:04 -0500

    complete

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,22 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+  it('should filter favorite foods of users', function () {
+        var faker = [{
+          name: 'Jaclyn',
+          favoriteFood: 'Olives'
+        }, {
+          name: 'Ore',
+          favoriteFood: 'Black Beans'
+        }, {
+          name: 'Joseph',
+          favoriteFood: 'French Toast Bagels'
+        }];
+
+    var results = $filter('favoriteFood')(faker, 'French Toast Bagels');
+
+    expect(results.length).toBe(1);
+    expect(results[0].name).toBe('Joseph');
+  });
 	
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -6,5 +6,11 @@ describe('removeAllVowels Filter', function () {
 	beforeEach(inject(function ($injector) {
 		$filter = $injector.get('$filter');
 	}));
+    it('should remove all vowels from a string', function () {
+
+    var results = $filter('removeAllVowels')("Ahoy maties, I'm a string of letters.");
+
+    expect(results).toEqual("hy mts, 'm  strng f lttrs.");
+  });
 
 });
